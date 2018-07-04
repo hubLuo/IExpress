7b8fd6a0a61c1fa78787e819eae97f70d0780ed4
hubLuo <hubluo@126.com>
2018-07-04 14:55:01 +0800

    1.实现express的请求监听listen 关键点是使用apply来传递未知个数的参数

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ *作者：luo
+ *时间：2018/7/4 0004
+ *Email：hubluo@gmail.com
+ *功能：应用界面
+ */
+const express=require("./lib/IExpress.js");
+const app=express();
+
+app.listen(3000,function(){
+    console.log("链接3000端口成功！");
+});
