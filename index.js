d1b3409f40e47e10fedf61c4b83516455b0c155a
hubLuo <hubluo@126.com>
2018-07-05 10:51:31 +0800

    单句柄声明式路由实现思路： 利用函数即是对象又是方法的特点       1对象属性挂载/扩展路由方法。       2方法体内执行路由方法堆栈。

--- index.js
+++ index.js
@@ -7,6 +7,10 @@
 const express=require("./lib/IExpress.js");
 const app=express();
 
+app.get('/user', function(req, res, next) {
+    res.end('二逼用户');
+});
+
 app.listen(3000,function(){
     console.log("链接3000端口成功！");
 });
